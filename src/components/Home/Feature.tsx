a32f0dbb4c10d4bba70ab42cca01c4a379e09127
JaimeGensler <jaimegensler0@gmail.com>
2023-06-04 11:23:18 -0700

    Remove comment

--- src/components/Home/Feature.tsx
+++ src/components/Home/Feature.tsx
@@ -18,21 +18,3 @@ export function Feature({ icon, title, children }: Props) {
 		</div>
 	);
 }
-
-// type Feature = [string, string];
-// //prettier-ignore
-// const features = [
-// 	['Safe', 'lock',],
-// 	['Multithreaded', '']
-// ]
-
-// -   A safety-first approach (no `eval` or eval-like constructs)
-// -   Hassle-free multithreading
-// -   Archetypal storage
-// -   Complex queries
-// -   Deeply customizable execution logic
-// -   Dynamically sized types
-// -   World-unique data
-// -   Event handling
-// -   Fine-grain memory management
-// -   Integration with third-party libraries
